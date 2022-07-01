8a8556e80957cf65b7a01432cf6334268a38455a
Valeria de Angel <71499884+valeriadeangel@users.noreply.github.com>
2022-07-01 15:30:29 +0100

    Add files via upload
    Restructured data adding the name of the file as a time stamp.

--- data_extraction/restructure_data.R
+++ data_extraction/restructure_data.R
@@ -17,63 +17,70 @@
 #############                                                     #############
 
 
-library(dplyr)
-library(readr)
+library(tidyverse)
 
-setwd("E:/")
+setwd("F:/")  # check that this is the correct path to the external hard drive.
 
-DESIRED_FEATURE="connect_fitbit_sleep_classic" # change features here
+DESIRED_FEATURE="questionnaire_qids" # change features here
+
 
 #creates a list of all the participants
-parent_lst<- list.files(path="./WinSCP files", pattern=NULL, all.files=FALSE, 
+participants<- list.files(path="./WinSCP files", pattern=NULL, all.files=FALSE, 
            full.names=TRUE)
 
 
-## Basic
-for(x in 1:length(parent_lst))
+## Loop for extraction
+for(x in 1:length(participants))
 {
-#creates a list of all the subfolders within the participants - so the features
-  sub_dir_list<-list.files(path=parent_lst[x], pattern="", all.files=FALSE,
+  #creates a list of all the subfolders within the participants - so the features
+  features<-list.files(path=participants[x], pattern="", all.files=FALSE,
                            full.names=TRUE)
   
   #generates folders where the features will be stored
-  folder_names <- list.files(path=parent_lst[x], pattern="", all.files=FALSE,
+  folder_names <- list.files(path=participants[x], pattern="", all.files=FALSE,
                              full.names=FALSE)
   
+  
   #Loop through the feature folders
-  for (y in 1:length(sub_dir_list))
+  for (y in 1:length(features))
   {
     #but only if folder name exists
     if (folder_names[y]==DESIRED_FEATURE)
-      {
-    sub_dir_list_df <- list.files(path=sub_dir_list[y], pattern="csv.gz", all.files=FALSE,
-                             full.names=TRUE)%>%
+    {
+      #creates the list for the name of the file (i.e. the date/time of the date point)
+      features_timestamp <- list.files(path=features[y], pattern="csv.gz", all.files=FALSE,
+                                           full.names=FALSE) %>%
+        str_replace(".csv.gz","")
       
-      #read the cvs files and bind them all rowwise
-      lapply(read_csv) %>% 
-      bind_rows %>%distinct
-    dir.create(file.path('./', DESIRED_FEATURE), showWarnings = FALSE)
-
-    #create a folder for each participant 
-    write_csv(sub_dir_list_df,paste("./",DESIRED_FEATURE,'/',folder_names[y],"_sub",x,".csv",sep=''))
-    #remove the lists of subfolders from R memory
-    rm(sub_dir_list_df)
+      #creates a list of the files themselves
+      features_files<- list.files(path=features[y], pattern="csv.gz", all.files=FALSE,
+                                      full.names=TRUE)
+      
+      #combines all the csv files, including the name as a variable
+      a <- Map(cbind, lapply(features_files, read_csv), timestamp = features_timestamp)
+      features_df <- Reduce(full_join,a) %>% distinct()
+      
+      #creates a folder in which to store these features
+      dir.create(file.path('./', DESIRED_FEATURE), showWarnings = FALSE)
+      #creates a file of concatenated csv files for all participants and stores here
+      write_csv(features_df,paste("./",DESIRED_FEATURE,'/',folder_names[y],"_sub",x,".csv",sep=''))
+      #removes the unwanted lists from R memory
+      rm(features_df)
     }
   }
   
 }
-# now create master file with all the subfolders from all partcipants together
+  
+  
+# now create master file with all the subfolders from all participants together
 master_file<- list.files(path=paste('./',DESIRED_FEATURE,sep=''), pattern=NULL, all.files=FALSE,
                         full.names=TRUE)%>% 
   lapply(read_csv) %>% 
   bind_rows %>%distinct
+
 #and create a csv file for it
-write_csv(master_file,paste("./",DESIRED_FEATURE,'/',"master_",DESIRED_FEATURE,".csv",sep=''))
+write_csv(master_file,paste("./feature folders/","master_",DESIRED_FEATURE,".csv",sep=''))
 
 # delete the files that are now joined
 junk <- list.files(path=paste('./',DESIRED_FEATURE,sep=''),  pattern="sub",full.names = TRUE)
 file.remove(junk,showWarnings = FALSE)
-
-
-
-
